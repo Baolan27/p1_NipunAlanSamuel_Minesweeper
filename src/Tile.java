ac707d3dd73e02e8e5a6532f5788871e9c494b4a
NipunDas <43912314+NipunDas@users.noreply.github.com>
2021-04-26 08:33:26 -0700

    initial commit

---
+++ src/Tile.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Tile {
+
+    ImageIcon coverImage; //initialize with generic tile image (same for all tiles)
+    ImageIcon uncoveredImage; //initialize in constructor depending on the type
+    ImageIcon display; //what the tile actually looks like
+
+    //0 = empty tile, 1 = number tile, 2 = mine tile
+    public Tile(int type) {
+        if (type == 0) {
+            //set uncovered image to an empty space
+        } else if (type == 1) {
+            //set uncovered image to a number
+        } else if (type == 2) {
+
+        }
+    }
+
+    public ImageIcon getTileImage() {
+        return display;
+    }
+
+
+}
