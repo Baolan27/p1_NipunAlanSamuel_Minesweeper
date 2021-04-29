f46a59a2635642a55cfc387bf4232c5e7b9a5df3
Baolan27 <alanxb621@gmail.com>
2021-04-29 09:16:47 -0700

    new classes

---
+++ src/GUI.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUI extends JPanel implements MouseListener, ActionListener {
+    public GUI() {
+
+    }
+
+    public static void main(String[] args) {
+        GUI g = new GUI();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
