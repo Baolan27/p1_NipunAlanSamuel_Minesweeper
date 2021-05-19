4744d5b59192972227db9b8897a08d9a2fafd81f
Baolan27 <alanxb621@gmail.com>
2021-05-19 09:03:37 -0700

    computeNo algo method pre-debugging phase completed, added more useful instance variables to Tile

--- src/Board.java
+++ src/Board.java
@@ -23,8 +23,8 @@ public class Board {
             for (int j = 0; j < w; j++) {
                 if (cMines < 40) {
                     int rand = (int)(Math.random()*10);
-                    if (rand == 2) {
-                        board[i][j] = new Tile(2);
+                    if (rand == 9) {
+                        board[i][j] = new Tile(9);
                         cMines++;
                     }
                     else board[i][j] = new Tile(0);
@@ -36,7 +36,63 @@ public class Board {
     }
 
     public void computeNo() {
-
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < w; j++) {
+                int count = 0;
+                if (board[i][j].getType() == 0) {
+                    if (i == 0 || i == l-1 || j == 0 || j == w - 1) {
+                        // test for corner cases
+                        if (i == 0 && j == 0) { // top left
+                            if (board[i][j+1].getType() == 9) count++;
+                            if (board[i+1][j+1].getType() == 9) count++;
+                            if (board[i+1][j].getType() == 9) count++;
+                        } else if (i == 0 && j == w-1) { // top right
+                            if (board[i][j-1].getType() == 9) count++;
+                            if (board[i+1][j-1].getType() == 9) count++;
+                            if (board[i+1][j].getType() == 9) count++;
+                        } else if (i == l-1 && j == 0) { // bottom left
+                            if (board[i][j+1].getType() == 9) count++;
+                            if (board[i-1][j+1].getType() == 9) count++;
+                            if (board[i-1][j].getType() == 9) count++;
+                        } else if (i == l-1 && j == w-1) { // bottom right
+                            if (board[i][j-1].getType() == 9) count++;
+                            if (board[i-1][j-1].getType() == 9) count++;
+                            if (board[i-1][j].getType() == 9) count++;
+                        } else {
+                            if (i == 0) {
+                                for (int I = i-1; I <= i+1; I++) {
+                                    if (board[I][j].getType() == 9) count++;
+                                    if (board[I][j+1].getType() == 9) count++;
+                                }
+                            } else if (i == l-1) {
+                                for (int I = i-1; I <= i+1; I++) {
+                                    if (board[I][j].getType() == 9) count++;
+                                    if (board[I][j-1].getType() == 9) count++;
+                                }
+                            } else if (j == 0) {
+                                for (int J = j-1; J <= j+1; J++) {
+                                    if (board[i][J].getType() == 9) count++;
+                                    if (board[i+1][J].getType() == 9) count++;
+                                }
+                            } else {
+                                for (int J = j-1; J <= j+1; J++) {
+                                    if (board[i-1][J].getType() == 9) count++;
+                                    if (board[i][J].getType() == 9) count++;
+                                }
+                            }
+                        }
+                    } else {
+                        // test 3x3 square around a given "0" tile
+                        for (int I = i-1; I <= i+1; I++) {
+                            for (int J = j-1; J <= j+1; J++) {
+                                if (board[I][J].getType() == 9) count++;
+                            }
+                        }
+                    }
+                    board[i][j] = new Tile(count);
+                }
+            }
+        }
     }
 
 }
--- src/Tile.java
+++ src/Tile.java
@@ -2,19 +2,29 @@ import javax.swing.*;
 
 public class Tile {
 
+    int type;
     ImageIcon coverImage; //initialize with generic tile image (same for all tiles)
     ImageIcon uncoveredImage; //initialize in constructor depending on the type
     ImageIcon display; //what the tile actually looks like
 
     //0 = empty tile, 1 = number tile, 2 = mine tile
     public Tile(int type) {
+        this.type = type;
         if (type == 0) {
             //set uncovered image to an empty space
-        } else if (type == 1) {
+        } else if (type >= 1 && type <= 8) {
             //set uncovered image to a number
-        } else if (type == 2) {
+        } else if (type == 9) {
             // mine
         }
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setUncoveredImage() {
+
     }
 
     public ImageIcon getTileImage() {
