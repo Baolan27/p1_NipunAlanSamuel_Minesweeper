e42b1ee325b35c013e930749c7c6615fbe0fa443
Baolan27 <alanxb621@gmail.com>
2021-05-27 09:00:23 -0700

    runnable

--- src/Board.java
+++ src/Board.java
@@ -9,6 +9,7 @@ public class Board {
         w = 16;
         m = 40;
         cMines = 0;
+        fillTemp();
     }
 
     public Board(int length, int width, int numMines) { //custom game, beginner (9,9,10), expert (16,30,99)
@@ -17,7 +18,7 @@ public class Board {
         m = numMines;
         cMines = 0;
     }
-
+/*
     public void fill() {
         for (int i = 0; i < l; i++) {
             for (int j = 0; j < w; j++) {
@@ -31,6 +32,30 @@ public class Board {
                 } else {
                     board[i][j] = new Tile(0);
                 }
+            }
+        }
+        if (cMines < 40) {
+            for (int i = 0; i < l; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (cMines < 40){
+                        if (board[i][i].getType() != 9) {
+                            int rand = (int)(Math.random()*10);
+                            if (rand == 9) {
+                                board[i][j] = new Tile(9);
+                                cMines++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ */
+
+    public void fillTemp() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = new Tile(0);
             }
         }
     }
@@ -93,6 +118,34 @@ public class Board {
                 }
             }
         }
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public Tile[][] getBoard() {
+        return board;
     }
 
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,35 +1,99 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.SwingUtilities;
 
-public class Tile {
+public class Tile extends JLabel{
 
+    private ImageIcon coverImage = new ImageIcon("src/Images/tile.png", "Uncovered tile image"); //initialize with generic tile image (same for all tiles)
+    private ImageIcon uncoveredImage; //initialize in constructor depending on the type
+    private ImageIcon display; //what the tile actually looks like
+    private boolean isFlagged = false;
     int type;
-    ImageIcon coverImage; //initialize with generic tile image (same for all tiles)
-    ImageIcon uncoveredImage; //initialize in constructor depending on the type
-    ImageIcon display; //what the tile actually looks like
 
     //0 = empty tile, 1 = number tile, 2 = mine tile
     public Tile(int type) {
         this.type = type;
         if (type == 0) {
             //set uncovered image to an empty space
+            uncoveredImage = new ImageIcon("src/Images/empty.png", "Empty tile image");
         } else if (type >= 1 && type <= 8) {
             //set uncovered image to a number
+            uncoveredImage = new ImageIcon("src/Images/" + type + ".png", "Number tile image");
         } else if (type == 9) {
-            // mine
+            //set uncovered image to a mine
+            uncoveredImage = new ImageIcon("src/Images/mine.png", "Tile with mine image");
         }
-    }
+        display = coverImage;
+        super.setIcon(display);
 
-    public int getType() {
-        return type;
-    }
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    revealTile();
+                } else if (SwingUtilities.isRightMouseButton(e)) {
+                    if (!isFlagged) {
+                        flagTile();
+                    } else {
+                        unFlagTile();
+                    }
+                }
+                System.out.println(type);
+            }
 
-    public void setUncoveredImage() {
+            @Override
+            public void mousePressed(MouseEvent e) {
 
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
     }
 
     public ImageIcon getTileImage() {
         return display;
     }
 
+    public void revealTile() {
+        display = uncoveredImage;
+        super.setIcon(display);
+    }
 
+    public void flagTile() {
+        //putting a flag on the tile
+        coverImage = new ImageIcon("src/Images/flag.png", "Tile with flag");
+        display = coverImage;
+        super.setIcon(display);
+        isFlagged = true;
+    }
+
+    public void unFlagTile() {
+        //removing the flag from a flagged tile
+        coverImage = new ImageIcon("src/Images/tile.png", "Uncovered tile image");
+        display = coverImage;
+        super.setIcon(display);
+        isFlagged = false;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public int getType() {
+        return type;
+    }
 }
