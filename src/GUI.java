c757ebf7608debd447b42f29e68a19cf5385d7df
samue <samue@Sfong-3.lan>
2021-06-07 21:56:59 -0700

    timer

--- src/GUI.java
+++ src/GUI.java
@@ -11,27 +11,60 @@ public class GUI extends JLabel implements MouseListener, ActionListener {
 	Board board;
 	JFrame frame;
 	Music bgm;
-
+	Timer timer;
+	//timer
+	 JLabel timeLabel = new JLabel();
+	 int elapsedTime = 0;
+	 int seconds =0;
+	 int minutes =0;
+	 int hours =0;
+	 boolean started = false;
+	 String seconds_string = String.format("%02d", seconds);
+	 String minutes_string = String.format("%02d", minutes);
+	 String hours_string = String.format("%02d", hours);
+	 
+	 
 	public GUI() {
-		//initializing board and label storage
+		
+		
+		Stopwatch stopwatch = new Stopwatch();;
+		
+		//initializing board and label storage  
 		board = new Board();
 		
 		//music
 		bgm = new Music("backgroundmusic.wav",true);
-		bgm.play();
+		//bgm.play();
 
 		//JFrame setup
 		frame = new JFrame("Minesweeper");
 		JPanel panel = new JPanel();
 		JPanel extraPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-		frame.setPreferredSize(new Dimension(750, 800));
-		frame.pack();
+		frame.setPreferredSize(new Dimension(750, 900));
+		
+		// timer
+		timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+		timeLabel.setBounds(100,100,200,100);
+		timeLabel.setFont(new Font("Verdana",Font.PLAIN,35));
+		timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+		timeLabel.setOpaque(true);
+		timeLabel.setHorizontalAlignment(JTextField.CENTER);
+		// start timer
+		timer.start();
+		
+		
+		
+		
+		
+		frame.pack();	
 		GridLayout layout = new GridLayout(0, board.getL());
 		layout.setHgap(3);
 		layout.setVgap(3);
 		frame.setLayout(layout);
 		frame.getContentPane().setBackground(Color.gray);
 		frame.add(this);
+		frame.add(timeLabel);
+		
 
 		//loop to create all labels based on board and add them to frame
 		for (int i = 0; i < board.getL(); i++) {
@@ -45,11 +78,27 @@ public class GUI extends JLabel implements MouseListener, ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Minesweeper");
 		frame.setVisible(true);
-
-		//timer stuff
-		Timer t = new Timer(16, this);
-		t.start();
+	
 	}
+	
+
+	
+	 Timer timer1 = new Timer(1000, new ActionListener() {
+		  
+		  public void actionPerformed(ActionEvent e) {
+		   
+		   elapsedTime=elapsedTime+1000;
+		   hours = (elapsedTime/3600000);
+		   minutes = (elapsedTime/60000) % 60;
+		   seconds = (elapsedTime/1000) % 60;
+		   seconds_string = String.format("%02d", seconds);
+		   minutes_string = String.format("%02d", minutes);
+		   hours_string = String.format("%02d", hours);
+		   timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+		   
+		  }
+		  
+		 });
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -91,6 +140,27 @@ public class GUI extends JLabel implements MouseListener, ActionListener {
 		}
 	}
 
+	 
+	 void start() {
+	  timer.start();
+	 }
+	 
+	 void stop() {
+	  timer.stop();
+	 }
+	 
+	 void reset() {
+	  timer.stop();
+	  elapsedTime=0;
+	  seconds =0;
+	  minutes=0;
+	  hours=0;
+	  seconds_string = String.format("%02d", seconds);
+	  minutes_string = String.format("%02d", minutes);
+	  hours_string = String.format("%02d", hours);       timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+	 }
+	
+	
 	public void paint(Graphics g) {
 		/*
 		frame.removeAll();
---
+++ src/Stopwatch.java
@@ -0,0 +1,82 @@
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.Timer;
+
+public class Stopwatch {
+	 
+	 JFrame frame = new JFrame();
+	 JLabel timeLabel = new JLabel();
+	 int elapsedTime = 0;
+	 int seconds =0;
+	 int minutes =0;
+	 int hours =0;
+	 boolean started = false;
+	 String seconds_string = String.format("%02d", seconds);
+	 String minutes_string = String.format("%02d", minutes);
+	 String hours_string = String.format("%02d", hours);
+	 
+	 Timer timer = new Timer(1000, new ActionListener() {
+	  
+	  public void actionPerformed(ActionEvent e) {
+	   
+	   elapsedTime=elapsedTime+1000;
+	   hours = (elapsedTime/3600000);
+	   minutes = (elapsedTime/60000) % 60;
+	   seconds = (elapsedTime/1000) % 60;
+	   seconds_string = String.format("%02d", seconds);
+	   minutes_string = String.format("%02d", minutes);
+	   hours_string = String.format("%02d", hours);
+	   timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+	   
+	  }
+	  
+	 });
+	 
+	 
+	 public Stopwatch(){
+	  
+	  timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+	  timeLabel.setBounds(100,100,200,100);
+	  timeLabel.setFont(new Font("Verdana",Font.PLAIN,35));
+	  timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+	  timeLabel.setOpaque(true);
+	  timeLabel.setHorizontalAlignment(JTextField.CENTER);
+	  
+	  
+	  frame.add(timeLabel);
+	  
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  frame.setSize(420,420);
+	  	frame.setLayout(null);
+	  frame.setVisible(true);
+	  timer.start();
+	 }
+	 
+	 void start() {
+	  timer.start();
+	 }
+	 
+	 void stop() {
+	  timer.stop();
+	 }
+	 
+	 void reset() {
+	  timer.stop();
+	  elapsedTime=0;
+	  seconds =0;
+	  minutes=0;
+	  hours=0;
+	  seconds_string = String.format("%02d", seconds);
+	  minutes_string = String.format("%02d", minutes);
+	  hours_string = String.format("%02d", hours);       timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+	 }
+
+	 
+
+}
